3baf234ad2f77c12fa72cc5a90c9bf6718e55984
bharat <bharat@bharat.myguest.virtualbox.org>
2024-02-20 14:53:02 +0530

    added controller

--- mainframe-simulator/src/main/java/com/example/mq/MqDataApplication.java
+++ mainframe-simulator/src/main/java/com/example/mq/MqDataApplication.java
@@ -1,14 +1,20 @@
 package com.example.mq;
 
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import com.example.mq.services.JsonToEbcdic;
+
 @SpringBootApplication
 public class MqDataApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MqDataApplication.class, args);
+		
 		System.out.println("spring project started at port : http://localhost:8080 ");
+		
 	}
 
 }
---
+++ mainframe-simulator/src/main/java/com/example/mq/controller/Simulator.java
@@ -0,0 +1,24 @@
+package com.example.mq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mq.services.JsonToEbcdic;
+
+@RestController
+@RequestMapping("/")
+public class Simulator {
+
+	@Autowired
+	private JsonToEbcdic j2e;
+	
+	private String cobolFile = "resources/customer.cpy";
+	
+	@GetMapping("send")
+	public byte[] jsonToEbcdic() {
+		return j2e.request2mainframe(cobolFile);
+	}
+	
+}
